e80f5b524ef7603d1834e4db38279a43f322a6d4
Nika Pavlič <nika.pavlic775@gmail.com>
2021-07-30 19:04:07 +0200

    float

--- model.py
+++ model.py
@@ -23,13 +23,13 @@ class Model:
         self.moja_zgodovina.append(skupina)
 
     def imena_skupin(self):
-        """Vrne seznam imen skupin."""
+        """Vrne seznam imen skupin napisanih z velikimi tiskanimi črkami."""
         seznam = [str(skupina.ime) for skupina in self.skupine]
         povecan_seznam = []
         for ime in seznam:
             povecan_seznam.append(ime.upper())
         return povecan_seznam
-        
+
     #def dodaj_udelezenca(self, ime):
     #    self.aktualna_skupina.dodaj_udelezenca(ime)
     #    self.udelezenci.append(ime)
@@ -87,13 +87,21 @@ class Skupina:
         return len(self.udelezenci)
 
     def skupni_strosek(self):
-        return sum([int(Udelezenec.placal(oseba)) for oseba in self.udelezenci]) 
+        return sum([float(Udelezenec.placal(oseba)) for oseba in self.udelezenci]) 
 
     def strosek_enega(self):
         if self.stevilo_udelezencev() == 0:
             return 0
         else:
             return round(self.skupni_strosek() / self.stevilo_udelezencev(), 2) 
+
+    def imena_udelezencev(self):
+        """Vrne seznam imen udeležencev v skupini napisanih z velikimi tiskanimi črkami."""
+        seznam = [str(udelezenec.ime) for udelezenec in self.udelezenci]
+        povecan_seznam = []
+        for ime in seznam:
+            povecan_seznam.append(ime.upper())
+        return povecan_seznam
 
     def v_slovar(self):
         return {
@@ -126,11 +134,11 @@ class Udelezenec:
         self.placila.remove(placilo)
 
     def placal(self):
-        return sum([int(placilo.znesek) for placilo in self.placila])
+        return sum([float(placilo.znesek) for placilo in self.placila])
 
     def še_dolzen(self):
         skupina = Model().aktualna_skupina
-        return int(Skupina.strosek_enega(skupina)) - int(self.placal())
+        return float(Skupina.strosek_enega(skupina)) - float(self.placal())
 
     def v_slovar(self):
         return {
